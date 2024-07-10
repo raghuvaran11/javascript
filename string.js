0c2e799b603a00f0753bcb2ce5851a9046b96b7e
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-07-10 11:02:12 +0530

    repractice

--- string.js
+++ string.js
@@ -3,7 +3,12 @@
 // var z= x+y
 // console.log(z)
 
-var x=10
-var x=2
-var z=x
-console.log(x)
+// var x=10
+// var x=2
+// var z=x
+// console.log(x)
+
+let x=10
+let y= 12
+let z= x+y
+console.log(z)
